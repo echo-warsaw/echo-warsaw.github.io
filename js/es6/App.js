e533adfcf72441e643bc1ffdb8d69af44e82ec61
Kacper Pietrzak <kacpietrzak@gmail.com>
2017-04-29 14:52:51 +0200

    Featuring modal that shows up on error

--- js/es6/App.js
+++ js/es6/App.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-console */
+
 import React from 'react';
 import UserInput from './components/UserInput';
+import ErrorModal from './components/ErrorModal';
 import Header from './components/Header';
 
 export default class App extends React.Component {
 	constructor( props ){
 		super( props );
 
+		this.state = {
+			shouldShowError: false
+		};
+
 		this.onSubmit = this.onSubmit.bind( this );
+		this.hideError = this.hideError.bind( this );
 	}
 	onSubmit( data ){
 		const url = '/api/new';
@@ -25,19 +33,27 @@ export default class App extends React.Component {
 			throw new Error( 'Query is inalid' );
 		} ).then( data => {
 			if ( data.statusCode === 400 ){
-			     throw new Error( 'Server response is far from ok' );
+				throw new Error( 'Server response is far from ok' );
 			}
 			console.log( 'Everything went alright! ( At least on frontend side ) )' );
-		} ).catch( () => {
+		} ).catch( error => {
 			console.error( 'There was some kind of problem with your request...' );
 			console.error( error );
+			this.showError();
 		} );
+	}
+	showError(){
+		this.setState( { shouldShowError: true } );
+	}
+	hideError(){
+		this.setState( { shouldShowError: false } );
 	}
 	render(){
 		return (
   <div>
     <Header />
     <UserInput onSubmit={this.onSubmit} />
+    <ErrorModal onHide={this.hideError} show={this.state.shouldShowError} />
   </div>
 		);
 	}
---
+++ js/es6/components/ErrorModal.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const ErrorModal = props => (
+  <Modal {...props} bsSize='small' aria-labelledby='modal-title' className='error-modal'>
+    <Modal.Header closeButton>
+      <Modal.Title id='modal-title'>Your request has been rejected :(</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <p>{'Try checking your internet connection or if your facebook page url is valid'}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button bsSize='xsmall' onClick={props.onHide}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+    );
+
+ErrorModal.propTypes = {
+	onHide: React.PropTypes.func
+};
+
+ErrorModal.defaultProps = {
+	onHide: () => console.log( 'Modal should hide' )
+};
+
+export default ErrorModal;
